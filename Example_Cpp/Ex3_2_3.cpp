ed53e93fefd02754aed0b1445604eae5d5defc65
DaYe Min <mindaye8504@gmail.com>
2020-09-09 20:58:08 +0900

    Ex 3-2-3 is now in progress

---
+++ Example_Cpp/Ex3_2_3.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+
+#include "setpos.cpp"
+
+#define X_MAX 79
+#define Y_MAX 24
+
+int main(void){
+	
+	char key;
+	int x = 10, y = 5;
+		
+	return 0;
+}
